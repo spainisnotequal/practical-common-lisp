42b831a77b7e7c91eab5e0f33756563a8b6b9fad
spainisnotequal <spainisnotequal@gmail.com>
2019-07-04 10:14:48 +0200

    Explain some useful sequence functions

--- chapter11/collections.lisp
+++ chapter11/collections.lisp
@@ -56,3 +56,46 @@
 ;; like this:
 (setf (elt *v* 1) -11) ; => -11
 *v*                    ; => #(-3 -11 5)
+
+;;; -----------------------------------------------------
+;;; Finding and filtering specific elements of a sequence
+;;; -----------------------------------------------------
+
+(count 1 '(1 2 3 1 2 3)) ; => 2
+
+(remove 1 #(1 2 3 1 2 3)) ; => #(2 3 2 3)
+(remove 1 '(1 2 3 1 2 3)) ; => (2 3 2 3)
+(remove #\a "foobarbaz")  ; => "foobrbz"
+
+(substitute 9 1 #(1 2 3 1 2 3))  ; => #(9 2 3 9 2 3)
+(substitute 9 1 '(1 2 3 1 2 3))  ; => (9 2 3 9 2 3)
+(substitute #\A #\a "foobarbaz") ; => "foobArbAz"
+
+(find 1 #(1 2 3 1 2 3)) ; => 1
+(find 1 '(1 2 3 1 2 3)) ; => 1
+(find #\a "foobarbaz")  ; => #\a
+
+(position 1 '(1 2 3 1 2 3)) ; => 0
+(position #\a "foobarbaz")  ; => 4
+
+;; We can use keyword arguments to modify the behaviour of these functions
+
+;; :test keyword
+(remove 2 '(1 2 3 1 2 3)
+        :test #'>=)       ; => (3 3)
+(remove 2 '(1 2 3 1 2 3)
+        :test #'(lambda (x y) (>= x y))) ; => (3 3)
+
+(remove 2 '(1 2 3 1 2 3)
+        :test (complement #'>=)) ; => (1 2 1 2)
+
+;; :key keyword
+(remove 2 '((1 "one") (2 "two") (3 "three"))
+        :key #'first)                        ; => ((1 "one") (3 "three"))
+(remove 2 '((1 "one") (2 "two") (3 "three"))
+        :test #'>=
+        :key #'first)                        ; => (3 "three")
+
+;; :from-end keyword
+(find 'a #((a 10) (b 20) (a 30) (b 40)) :key #'first)             ; => (A 10)
+(find 'a #((a 10) (b 20) (a 30) (b 40)) :key #'first :from-end t) ; => (A 30)
