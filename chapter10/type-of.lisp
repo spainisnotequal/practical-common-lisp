d161f55dcad051a7691c9574b83e322b3fd22b86
spainisnotequal <spainisnotequal@gmail.com>
2019-07-07 20:38:00 +0200

    Use TYPE-OF to get the type of an object

---
+++ chapter10/type-of.lisp
@@ -0,0 +1,15 @@
+;;;; ---------------- ;;;;
+;;;; TYPE-OF function ;;;;
+;;;; ---------------- ;;;;
+
+(type-of 1)        ; => BIT
+(type-of 2)        ; => (INTEGER 0 536870911)
+(type-of 1/2)      ; => RATIO
+(type-of 3.14159)  ; => SINGLE_FLOAT
+
+(type-of #\a)      ; => STANDARD-CHAR
+(type-of 'foo)     ; => SYMBOL
+(type-of "foo")    ; => (SIMPLE-ARRAY CHARACTER (3))
+
+(type-of '(1 2 3)) ; => CONS
+(type-of #(1 2 3)) ; => (SIMPLE-VECTOR 3)
