d0bb6e71ce2980df1f3b5d81d75928e8d09e9500
spainisnotequal <spainisnotequal@gmail.com>
2020-04-19 12:36:21 +0200

    Read and write null-terminated ASCII string

--- chapter24/parsing-binary-files.lisp
+++ chapter24/parsing-binary-files.lisp
@@ -20,3 +20,18 @@
 (defun write-u2 (out value)
   (write-byte (ldb (byte 8 8) value) out)
   (write-byte (ldb (byte 8 0) value) out))
+
+;;; Strings in binary files
+
+(defconstant +null+ (code-char 0))
+
+(defun read-null-terminated-ascii (in)
+  (with-output-to-string (s)
+    (loop for char = (code-char (read-byte in))
+       until (char= char +null+) do (write-char char s))))
+
+(defun write-null-terminated-ascii (string out)
+  (loop for char across string
+     do (write-byte (char-code char) out))
+  (write-byte (char-code +null+) out))
+
